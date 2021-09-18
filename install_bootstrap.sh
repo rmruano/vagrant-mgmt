bf1274d7ee90d2bb7c8d69befc86bff3956022a3
ruben <ruben.moraleda.ruano@gmail.com>
2021-09-18 21:26:13 +0200

    Fix local bootstrap path

--- install_bootstrap.sh
+++ install_bootstrap.sh
@@ -6,11 +6,11 @@ echo "I: Running bootstrap"
 if [ ! -e /home/vagrant/provision/.bootstrap_done ]; then
  	
   # Run overrides bootstrap if exists ---------------------------------------
-	echo "I: Running /home/vagrant/provision/cfg_defaults/bootstrap.sh if exists"
-	if [ -e /home/vagrant/provision/cfg_defaults/bootstrap.sh ]; then
-		sudo chmod +x /home/vagrant/provision/cfg_defaults/bootstrap.sh;
-		sudo -u vagrant /home/vagrant/provision/cfg_defaults/bootstrap.sh
-		echo "I: completed running the /home/vagrant/provision/cfg_defaults/bootstrap.sh"
+	echo "I: Running /home/vagrant/provision/cfg/bootstrap.sh if exists"
+	if [ -e /home/vagrant/provision/cfg/bootstrap.sh ]; then
+		sudo chmod +x /home/vagrant/provision/cfg/bootstrap.sh;
+		sudo -u vagrant /home/vagrant/provision/cfg/bootstrap.sh
+		echo "I: completed running the /home/vagrant/provision/cfg/bootstrap.sh"
 	fi
 	
 	touch /home/vagrant/provision/.bootstrap_done
